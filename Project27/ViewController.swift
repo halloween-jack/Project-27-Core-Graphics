b451bdc7c98242141a840e0602dc8d2ed333ab9e
halloween-jack <halloween.jack.dev@gmail.com>
2017-11-03 04:48:36 +0900

    Images and text

--- Project27/ViewController.swift
+++ Project27/ViewController.swift
@@ -126,6 +126,31 @@ class ViewController: UIViewController {
         
         imageView.image = img
     }
+    
+    func drawImageAndText() {
+        // 1
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            // 2
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            // 3
+            let attrs = [NSAttributedStringKey.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSAttributedStringKey.paragraphStyle: paragraphStyle]
+            
+            // 4
+            let string: NSString = "The best-laid schemes o'\nmice an' men gang aft agley"
+            string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
+            
+            // 5
+            let mouse = UIImage(named: "mouse")
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        }
+        
+        // 6
+        imageView.image = img
+    }
 
     @IBAction func redrawTapped(_ sender: UIButton) {
         currentDrawType += 1
@@ -149,6 +174,9 @@ class ViewController: UIViewController {
             
         case 4:
             drawLines()
+            
+        case 5:
+            drawImageAndText()
             
         default:
             break
